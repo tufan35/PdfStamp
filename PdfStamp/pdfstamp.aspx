299933e3201630f49d8c2cc6366952412d1f0b46
tufan35 <34610153+tufan35@users.noreply.github.com>
2021-01-27 19:20:09 +0300

    Add project files.

---
+++ PdfStamp/pdfstamp.aspx
@@ -0,0 +1,75 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="pdfstamp.aspx.cs" Inherits="PdfStamp.pdfstamp1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+    <script>    
+            function printTrigger(elementId) {
+            var getMyFrame = document.getElementById(elementId);
+            getMyFrame.focus();
+            getMyFrame.contentWindow.print();
+        }
+    </script>
+   
+    <style type="text/css">
+        .auto-style1 {
+            width: 23%;
+        }
+    </style>
+   
+</head>
+<body style="height: 810px; width: 1107px">
+    <form id="form1" runat="server">
+           <div>
+
+            <table style="width:100%">
+                <tr>
+                    <td style="width:25%">
+                        <asp:FileUpload ID="fuUpload" runat="server" Width="90%" />
+                    </td>
+                    <td style="width:25%">
+                        <asp:Button ID="btnUpload" Text="Yükle" runat="server"  Width="90%" OnClick="Upload"/>
+                     </td>
+                    <td class="auto-style1">
+                        <asp:Button ID="btnView" runat="server" Text="Göster" OnClick="View" Width="90%" />
+                    </td>
+                    <td style="width:25%">
+                        <asp:Button ID="btnDownload" runat="server" OnClick="download" Text="İndir" Width="90%" />
+                    </td>
+                </tr>
+                <tr>
+                     <td style="width:25%">
+                        <input type="button" value="Print" onclick="printTrigger('frm_docs');"  />
+                    </td>
+                    <td style="width:25%">
+                        <asp:Button ID="btnstamp" runat="server" OnClick="Btnstamp" Text="Damga Bas!" Width="90%" />
+                    </td>
+                    <td class="auto-style1">
+                         <asp:ListBox ID="ListBox2" runat="server" Width="97px" SelectionMode ="Multiple"  >
+                            <asp:ListItem Text="deneme1" Value="//stampimage//deneme1.jpg"/>
+                            <asp:ListItem Text="deneme2" Value="//stampimage//deneme2.jpg"/>
+                            <asp:ListItem Text="deneme3" Value="//stampimage//deneme3.jpg"/>
+                            <asp:ListItem Text="deneme4" Value="//stampimage//deneme4.jpg"/>
+                            <asp:ListItem Text="deneme5" Value="//stampimage//deneme5.jpg"/>
+                            <asp:ListItem Text="deneme6" Value="//stampimage//deneme6.jpg"/>
+                            <asp:ListItem Text="deneme7" Value="//stampimage//deneme7.jpg"/>
+                        </asp:ListBox>
+
+                    </td>
+
+                  
+                </tr>
+                <tr>
+                    <td style="width:100%" colspan="4">
+                 <iframe runat="server" id="frm_docs" src="MDPdfKontrol.aspx" style="display:inline; border:solid 1px black; width:100%; height:800px;" > </iframe>
+                    </td>
+                </tr>       
+            </table>
+                <asp:Label ID="Label1" runat="server"></asp:Label>  
+        </div>
+    </form>
+</body>
+</html>
---
+++ PdfStamp/pdfstamp.aspx.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System.IO;
+using Image = iTextSharp.text.Image;
+
+namespace PdfStamp
+{
+    public partial class pdfstamp1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Upload(object sender, EventArgs e)
+        {
+            if (fuUpload.HasFile)
+            {
+
+                fuUpload.SaveAs(Server.MapPath("~//Upload//" + fuUpload.FileName));
+            }
+            else
+            {
+                Label1.Text = "Yüklenmedi";
+            }
+        }
+
+        protected void download(object sender, EventArgs e)
+        {
+            string dosyayolu = Server.MapPath("~//Upload//teststamp.pdf");
+            FileInfo file = new FileInfo(dosyayolu);
+            if (file.Exists)
+            {
+
+
+                Response.ContentType = "Application/pdf";
+                Response.AppendHeader("Content-Disposition", "attachment; filename=teststamp.pdf");
+                Response.TransmitFile(Server.MapPath("~//Upload//teststamp.pdf"));
+                Response.End();
+            }
+            else
+            {
+                Label1.Text = "indirilmedi.";
+            }
+        }
+
+        protected void View(object sender, EventArgs e)
+        {
+            string Test = "https://localhost:44374/Upload/teststamp.pdf";
+            frm_docs.Src = Test;
+
+
+
+
+        }
+
+
+        protected void Btnstamp(object sender, EventArgs e)
+        {
+
+            damga("C://Users//tufanc//source//repos//PdfStamp//PdfStamp//stampimage", "C://Users//tufanc//source//repos//PdfStamp//PdfStamp//Upload//teststamp.pdf");
+
+
+
+        }
+
+        #region Damga Ekleme (Stamp)
+
+        private void damga(string resimdosyası, string dosyayolu)
+        {
+
+
+            byte[] bytes = File.ReadAllBytes(dosyayolu);
+
+            PdfContentByte waterMark;
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+
+
+                PdfReader reader = new PdfReader(bytes);
+                using (PdfStamper stamper = new PdfStamper(reader, stream))
+                {
+
+
+                    int sayfa = reader.NumberOfPages;
+                    for (int j = 1; j <= sayfa; j++)
+                    {
+                        int imzax = 1;
+                        int imzay = 0;
+
+
+                        waterMark = stamper.GetUnderContent(j);
+
+
+                        for (int isim = 0; isim < ListBox2.Items.Count; isim++)
+                        {
+
+
+                            if (ListBox2.Items[isim].Selected)
+
+                            {
+                                string yol = Server.MapPath(ListBox2.Items[isim].Value);
+                                var resim = iTextSharp.text.Image.GetInstance(yol);
+
+                                imzax = imzax + 70;
+                                imzay = imzay + 0;
+
+                                resim.SetAbsolutePosition(imzax, imzay);
+
+
+                                waterMark.AddImage(resim);
+
+                            }
+
+                        }
+
+                    }
+
+                }
+                bytes = stream.ToArray();
+            }
+            File.WriteAllBytes(dosyayolu, bytes);
+
+        }
+
+    #endregion
+
+    }
+}
